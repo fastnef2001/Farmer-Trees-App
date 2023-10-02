8c51be05bc354e8ebf1b5da618bf0e2a7c0e8d7d
fastnef2001 <tnhanh5@gmail.com>
2023-10-02 16:49:27 +0700

    nhanh

--- src/screens/Statistics/ExpenseHistory.screen.tsx
+++ src/screens/Statistics/ExpenseHistory.screen.tsx
@@ -2,12 +2,145 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { HeaderTitle } from '../../components/Header/Header.component';
+import { ModalAdd } from '../../components/Modal/ModalAdd';
+import { UseLogic } from './UserLogic';
+import { HandleAdd } from './HandleAdd';
+import { ModalPickDate } from '../../components/Modal/ModalPickDate';
+import { ModalPick } from '../../components/Modal/ModalPick';
+import {
+  PopUpSuccess,
+  PopUpLoading,
+} from '../../components/Modal/GeneralPopUps.component';
 
 const ExpenseHistory = () => {
+  const {
+    selectedDateStart,
+    setSelectedDateStart,
+    selectedDateEnd,
+    setSelectedDateEnd,
+    handlePickDate,
+    handleReload,
+    totalIncome,
+    totalExpense,
+    totalProfit,
+    dataIncome,
+    dataExpense,
+    status,
+    isModalPickDate,
+    setIsModalPickDate,
+  } = UseLogic();
+  const {
+    isModaAdd,
+    setIsModalAdd,
+    titleModalAdd,
+    setTitleModalAdd,
+    inputs,
+    setInputs,
+    handleHideModalAdd,
+    handleInputChange,
+    inputsIncome,
+    setInputsIncome,
+    isDisabled,
+    setIsDisabled,
+    handleModalAddIncome,
+    inputsExpense,
+    setInputsExpense,
+    handleModalAddExpense,
+    unitsIncome,
+    trees,
+    costTypes,
+    unitsExpense,
+    handleModalPickHide,
+    handleModalPickTree,
+    handleModalPickUnitIncome,
+    handleModalPickCostType,
+    handleModalPickUnitExpense,
+    isModalPick,
+    setIsModalPick,
+    titlePick,
+    setTitlePick,
+    valuePick,
+    setValuePick,
+    hanleHideModalPick,
+    selectedDateIncome,
+    setSelectedDateIncome,
+    selectedDateExpense,
+    setSelectedDateExpense,
+    handleAdd,
+    isModalSuccess,
+    setIsModalSuccess,
+    titleHeader,
+    setTitleHeader,
+    titleBody,
+    setTitleBody,
+    isModalLoading,
+    setIsModalLoading,
+  } = HandleAdd();
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <HeaderTitle title="Expense history" />
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <HeaderTitle title="Expense history" onPress={handleModalAddExpense} />
+      </SafeAreaView>
+      {/* Modal pick date */}
+      <ModalPickDate
+        isModalPickDate={isModalPickDate}
+        setIsModalPickDate={setIsModalPickDate}
+        status={status}
+        setSelectedDateStart={setSelectedDateStart}
+        setSelectedDateEnd={setSelectedDateEnd}
+        setSelectedDateIncome={setSelectedDateIncome}
+        setSelectedDateExpense={setSelectedDateExpense}
+        selectedDateStart={selectedDateStart}
+        selectedDateEnd={selectedDateEnd}
+        selectedDateIncome={selectedDateIncome}
+        selectedDateExpense={selectedDateExpense}
+      />
+      {/* End modal pick date */}
+
+      {/* Modal add */}
+      <ModalAdd
+        isModaAdd={isModaAdd}
+        handleHideModalAdd={handleHideModalAdd}
+        titleModalAdd={titleModalAdd}
+        handlePickDate={handlePickDate}
+        selectedDateIncome={selectedDateIncome}
+        selectedDateExpense={selectedDateExpense}
+        handleModalPickTree={handleModalPickTree}
+        handleModalPickUnitIncome={handleModalPickUnitIncome}
+        handleModalPickUnitExpense={handleModalPickUnitExpense}
+        handleModalPickCostType={handleModalPickCostType}
+        handleInputChange={handleInputChange}
+        handleAdd={handleAdd}
+        inputs={inputs}
+        isDisabled={isDisabled}
+      />
+      {/* End modal add */}
+      {/* Modal pick */}
+      <ModalPick
+        isModalPick={isModalPick}
+        setIsModalPick={setIsModalPick}
+        titlePick={titlePick}
+        setTitlePick={setTitlePick}
+        valuePick={valuePick}
+        setValuePick={setValuePick}
+        trees={trees}
+        unitsIncome={unitsIncome}
+        costTypes={costTypes}
+        unitsExpense={unitsExpense}
+        handleModalPickHide={handleModalPickHide}
+        hanleHideModalPick={hanleHideModalPick}
+      />
+      {/* End modal pick */}
+      {/* Pop up noti and loading */}
+      <PopUpSuccess
+        isModalSuccess={isModalSuccess}
+        titleHeader={titleHeader}
+        titleBody={titleBody}
+      />
+      <PopUpLoading isModalSuccess={isModalLoading} />
+      {/* End up noti and loading */}
+    </>
   );
 };
 
