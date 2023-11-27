7efa88cacc0ee96a06d7964e1530800708ef8edb
fastnef2001 <tnhanh5@gmail.com>
2023-11-28 01:16:53 +0700

    nhanh

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import AppNavigator from './navigation/navigation';
-import { Provider } from 'react-redux';
-import { store } from './stores/index';
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <AppNavigator />
-    </Provider>
-  );
+  return <AppNavigator />;
 };
 
 export default App;
